e5ed1e8a42792af13534806a15e8e127082eca5f
Marcell Turi (MTUR.UK) <MTUR@columbusglobal.com>
2018-08-24 11:38:10 +0100

    SQL Script if you can't run PowerShell

---
+++ SQL/GetRecordCount.sql
@@ -0,0 +1,11 @@
+	
+SELECT SCHEMA_NAME(schema_id) AS [SchemaName],
+[Tables].name AS [TableName],
+SUM([Partitions].[rows]) AS [TotalRowCount]
+FROM sys.tables AS [Tables]
+JOIN sys.partitions AS [Partitions]
+ON [Tables].[object_id] = [Partitions].[object_id]
+AND [Partitions].index_id IN ( 0, 1 )
+GROUP BY SCHEMA_NAME(schema_id), [Tables].name
+
+-- WHERE [Tables].name = N'name of the table'
